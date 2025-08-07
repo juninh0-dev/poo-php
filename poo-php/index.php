6ced0f848db6abf521883a90f76c4f6c212569fd
Altair Preto de Godoi Junior <altair.godoi@fatec.sp.gov.br>
2025-08-06 22:41:28 -0300

    Add mais um input

--- poo-php/index.php
+++ poo-php/index.php
@@ -1,7 +1,11 @@
 <?php
     $nome = $_GET["nome"];
+    $altura = $_GET["altura"];
 
     print "Seja bem-vindo $nome!<br> 
-    Sua idade é {$_GET['idade']}";
+    Sua idade é {$_GET['idade']}<br>";
+
+    is_double($altura);
+    echo "E sua altura é: $altura";
 
 ?>
